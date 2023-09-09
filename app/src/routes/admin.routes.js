c3f28d37afbe249fe5894c969894bd577c107c41
19harshmehta <mehtah631@gmail.com>
2023-09-09 11:53:08 +0530

    category

--- app/src/routes/admin.routes.js
+++ app/src/routes/admin.routes.js
@@ -4,8 +4,11 @@ const authMiddleware = require("../middleware/auth.middleware")
 
 const router = express.Router()
 
-router.post("/addcategory",authMiddleware,categoryController.addCategoty)
-router.get("/getallcategory",authMiddleware,categoryController.getAllCategories)
+// router.post("/addcategory",authMiddleware,categoryController.addCategoty)
+// router.get("/getallcategory",authMiddleware,categoryController.getAllCategories)
+router.post("/addcategory",categoryController.addCategoty)
+router.get("/getallcategory",categoryController.getAllCategories)
+
 router.delete("/deletecategory/:categoryId",authMiddleware,categoryController.deleteCategoryById)
 
 
