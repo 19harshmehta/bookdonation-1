b2715a3bc30b8dad39d953e9ccc9eb0d12e35c1b
19harshmehta <mehtah631@gmail.com>
2023-09-15 17:28:48 +0530

    item crud uncomplete

--- app/src/routes/admin.routes.js
+++ app/src/routes/admin.routes.js
@@ -1,17 +1,22 @@
 const express = require("express")
 const categoryController = require("../controller/categotyController")
 const authMiddleware = require("../middleware/auth.middleware")
+const ItemController = require("../controller/itemController")
 
 const router = express.Router()
 
 // router.post("/addcategory",authMiddleware,categoryController.addCategoty)
 // router.get("/getallcategory",authMiddleware,categoryController.getAllCategories)
+// Category table
 router.post("/addcategory",categoryController.addCategoty)
 router.get("/getallcategory",categoryController.getAllCategories)
+router.delete("/deletecategory/:categoryId",categoryController.deleteCategoryById)
 
-router.delete("/deletecategory/:categoryId",authMiddleware,categoryController.deleteCategoryById)
+//Item table
 
-
+router.post("/additem",ItemController.addItem)
+router.get("/getallitem",ItemController.getAllItems)
+router.delete("/deleteitem/:itemId",ItemController.deleteItemById)
 
 
 module.exports = router
